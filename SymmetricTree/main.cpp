4252c76cbf045f2a1370e25bb31859663feb0cd5
honggengwei <honggengwei@gmail.com>
2014-11-01 20:54:18 +0800

    update

--- SymmetricTree/main.cpp
+++ SymmetricTree/main.cpp
@@ -15,74 +15,9 @@
 #include<iostream>
 #include<vector>
 #include<queue>
+#include "../test/test.h"
+
 using namespace std;
-
-
-/**
- * Definition for binary tree
- */
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
-TreeNode* OJTree(const char *str){
-    /*
-     *
-     *   1
-     *  / \
-     * 2   3
-     *    /
-     *   4
-     *    \
-     *     5
-     *
-     * 1,2,3,#,#,4,#,#,5
-     *
-     */
-    TreeNode* root = NULL;
-    if (*str == '\0'){
-        return root;
-    }
-
-    int val = *str++;
-    root = new TreeNode(val);
-
-    queue<TreeNode*> Queue;
-    Queue.push(root);
-
-    while (Queue.size()){
-        TreeNode* node = Queue.front();
-        Queue.pop();
-
-        if (node == NULL){
-            continue;
-        }
-        if (*str == '\0'){
-            return root;
-        }
-
-        if (*str == '#'){
-            node->left = NULL;
-        } else {
-            node->left = new TreeNode(*str);
-        }
-        str++;
-        if (*str == '#'){
-            node->right = NULL;
-        } else {
-            node->right = new TreeNode(*str);
-        }
-        str++;
-        Queue.push(node->left);
-        Queue.push(node->right);
-    }
-    return root;
-}
-
-
 /*
  * 最初思路是层级递归后看结果是否对称,折腾好久各种小问题没搞定
  * 最后才看到这种解法,原来只需要逻辑性旋转一下两个节点即可
@@ -106,13 +41,25 @@ class Solution {
 
 int main() {
     Solution s;
+    const char * str;
 
-    cout<<"123##4##5"<<"\t"<<s.isSymmetric(OJTree("123##4##5"))<<"\tfalse"<<endl;
-    cout<<"3445##56##6"<<"\t"<<s.isSymmetric(OJTree("3445##56##6"))<<"\ttrue"<<endl;
-    cout<<""<<"\t"<<s.isSymmetric(OJTree(""))<<"\ttrue"<<endl;
-    cout<<"123"<<"\t"<<s.isSymmetric(OJTree("123"))<<"\tfalse"<<endl;
-    cout<<"2334554##89##98"<<"\t"<<s.isSymmetric(OJTree("2334554##89##98"))<<"\tfalse"<<endl;
-    cout<<"12"<<"\t"<<s.isSymmetric(OJTree("12"))<<"\tfalse"<<endl;
+    str = "1,2,3,#,#,4,#,#,5";
+    cout<<str<<"\t"<<s.isSymmetric(OJTree(str))<<"\tfalse"<<endl;
+
+    str = "3,4,4,5,#,#,5,6,#,#,6";
+    cout<<str<<"\t"<<s.isSymmetric(OJTree(str))<<"\ttrue"<<endl;
+
+    str = "";
+    cout<<str<<"\t"<<s.isSymmetric(OJTree(str))<<"\ttrue"<<endl;
+
+    str = "1,2,3";
+    cout<<str<<"\t"<<s.isSymmetric(OJTree(str))<<"\tfalse"<<endl;
+
+    str = "2,3,3,4,5,5,4,#,#,8,9,#,#,9,8";
+    cout<<str<<"\t"<<s.isSymmetric(OJTree(str))<<"\tfalse"<<endl;
+
+    str = "1,2";
+    cout<<str<<"\t"<<s.isSymmetric(OJTree(str))<<"\tfalse"<<endl;
 }
 
 
